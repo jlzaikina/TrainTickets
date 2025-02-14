f4a911e366cc226dede004a1c0b96a4a05411c75
ZaikinaYS <zaikinajulia56@gmail.com>
2025-02-14 12:03:55 +0400

    Добавление SQL

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,14 +1,14 @@
-﻿-- public."User" определение
-
--- Drop table
-
--- DROP TABLE public."User";
-
-CREATE TABLE public."User" (
-	"Id" int4 NOT NULL,
+﻿CREATE TABLE public."User" (
+	"Id" int8 DEFAULT nextval('"User_id_seq"'::regclass) NOT NULL,
 	"Login" varchar(8) NOT NULL,
-	"Password" int4 NOT NULL,
-	"Email" varchar(20) NOT NULL,
-	"Passport" int4 NULL,
-	CONSTRAINT user_pk PRIMARY KEY ("Id")
+	"Password" bpchar(4) NOT NULL,
+	"Surname" varchar(30) NOT NULL,
+	"Name" varchar(20) NOT NULL,
+	"Email" varchar(40) NOT NULL,
+	"Phone" bpchar(11) NOT NULL,
+	CONSTRAINT user_check1 CHECK ((length(("Login")::text) > 3)),
+	CONSTRAINT user_pk PRIMARY KEY ("Id"),
+	CONSTRAINT user_unique UNIQUE ("Login"),
+	CONSTRAINT user_unique_1 UNIQUE ("Email"),
+	CONSTRAINT user_unique_2 UNIQUE ("Phone")
 );
